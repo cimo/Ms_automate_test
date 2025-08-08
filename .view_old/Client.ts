a957c6ff5e8df61c2a987600cbcdda5f95c89711
simone.dagostino <simone.dagostino@jp.kpmg.com>
2025-08-08 19:58:55 +0900

    update

---
+++ .view_old/Client.ts
@@ -0,0 +1,41 @@
+import { Iview } from "../JsMvcFwInterface";
+
+// Source
+import { IvariableList } from "../model/Index";
+
+const viewClient = (variableList: IvariableList): Iview => {
+    const clientListState = variableList.clientList.state;
+
+    const template = String.raw`
+    <table class="table_client">
+        <tbody>
+            <tr class="row not_hover">
+                <td class="cell">
+                    <p>
+                        Client connected:
+                    </p>
+                    <ul class="item" data-bind="clientList">
+                        ${(() => {
+                            const result: string[] = [];
+
+                            for (const [, value] of Object.entries(clientListState)) {
+                                result.push(`<li>
+                                    <i class="mdc-button__icon material-icons" aria-hidden="true">person</i>
+                                    <p>${value}</p>
+                                </li>`);
+                            }
+
+                            return result.join("");
+                        })()}
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>`;
+
+    return {
+        template
+    };
+};
+
+export default viewClient;
---
+++ .view_old/Upload.ts
@@ -0,0 +1,44 @@
+import { Iview } from "../JsMvcFwInterface";
+
+// Source
+
+const viewUpload = (): Iview => {
+    const template = String.raw`
+    <table class="table_upload">
+        <tbody>
+            <tr class="row not_hover">
+                <td class="cell">
+                    <p>
+                        Upload:<br /><br />
+                        This is a temporary file and will be delete on deploy
+                        phase.<br />If you need a stable file you need
+                        create a PR.
+                    </p>
+                    <div class="field_container">
+                        <button class="mdc-button mdc-button--raised mdc-button--leading button_primary button_input_upload_fake">
+                            Choose file
+                        </button>
+                        <input class="input_upload" type="file" value="" />
+                    </div>
+                    <div class="mdc-touch-target-wrapper">
+                        <button class="mdc-button mdc-button--raised mdc-button--leading button_primary button_upload">
+                            <span class="mdc-button__ripple"></span>
+                            <span class="mdc-button__label">
+                                <i class="mdc-button__icon material-icons"
+                                    aria-hidden="true">
+                                    upload
+                                </i> Upload
+                            </span>
+                        </button>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>`;
+
+    return {
+        template
+    };
+};
+
+export default viewUpload;
