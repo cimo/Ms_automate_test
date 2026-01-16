d051e99d58fef79e3b5ccf546a4cdaabb77caae8
simone.dagostino <simone.dagostino@jp.kpmg.com>
2026-01-16 21:25:18 +0900

    Update

--- script/playwright.sh
+++ script/playwright.sh
@@ -4,8 +4,8 @@ eval "$(dbus-launch --auto-syntax)"
 
 if [ "${1}" = "ui" ]
 then
-    npx playwright test --ui --config="${PATH_ROOT}src/playwright.config.ts"
+    npx -y playwright test --ui --config="${PATH_ROOT}src/playwright.config.ts"
 elif [ "${1}" = "code" ]
 then
-    npx playwright codegen
+    npx -y playwright codegen
 fi
